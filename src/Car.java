1fe9b10d47f0b4f893955f3b8937b51738c21366
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-07 12:14:35 +0200

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car extends Vehicle {
+    private int year;
+
+    public Car(String make, String model, int year) {
+        super(make, model);
+        this.year = year;
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    private String make;
+    private String model;
+
+    public Vehicle(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+}
