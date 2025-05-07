aba39dc35d5ed7dfae2817b7c335870f48b9c89f
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-07 12:35:50 +0200

    Student

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String name;
+    private int grade;
+
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public Student setGrade(int Grade) {
+        this.grade = grade;
+        return this;
+    }
+}
